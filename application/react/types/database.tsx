de4459e2ce8ee55ed96abe42ee50ef63fb29b1a8
deadlightreal <richardfabianmain@gmail.com>
2024-06-25 09:53:01 +0200

    added types

---
+++ application/react/types/database.tsx
@@ -0,0 +1,8 @@
+import Table from "./table";
+
+type Database = {
+  name: string;
+  tables: Array<Table>;
+}
+
+export default Database;
---
+++ application/react/types/table.tsx
@@ -0,0 +1,5 @@
+type Table = {
+  name: string;
+};
+
+export default Table;
